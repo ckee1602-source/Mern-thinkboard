5e83bfe2c119aaa25ae6ae624f4639063b5da60d
ckee1602-source <ckee1602@gmail.com>
2026-04-30 17:33:54 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express ();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 10000;
 
 const __dirname = path.resolve();
 
